e933a6245eab9cd1e4ad2ee760e09a01e223e4a7
alemaulou <alemau@uw.edu>
2018-11-07 15:11:20 -0800

    lab

---
+++ app/src/main/java/edu/uw/memorylab/BirdActivity.kt
@@ -0,0 +1,44 @@
+package edu.uw.memorylab
+
+import android.content.Intent
+import android.graphics.drawable.Drawable
+import android.os.Bundle
+import android.support.v4.content.ContextCompat
+import android.support.v7.app.AppCompatActivity
+import android.view.View
+import android.widget.ImageView
+
+class BirdActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_bird)
+
+        supportActionBar!!.setDisplayHomeAsUpEnabled(true)
+
+        findViewById(R.id.btnBirdNext)!!.setOnClickListener { startActivity(Intent(this@BirdActivity, BlankBirdActivity::class.java)) }
+
+        /* Set up image */
+        val image = ContextCompat.getDrawable(this, R.drawable.hummingbird) //get the drawable resource
+        val birdView = findViewById(R.id.imgBird) as ImageView?
+        birdView!!.setImageDrawable(image)
+    }
+
+    override fun onStop() {
+        super.onStop()
+        val image = ContextCompat.getDrawable(this, R.drawable.hummingbird) //get the drawable resource
+        val birdView = findViewById(R.id.imgBird) as ImageView?
+        birdView!!.setImageDrawable(null)
+
+    }
+
+    override fun onStart() {
+        super.onStart()
+        /* Set up image */
+        val image = ContextCompat.getDrawable(this, R.drawable.hummingbird) //get the drawable resource
+        val birdView = findViewById(R.id.imgBird) as ImageView?
+        birdView!!.setImageDrawable(image)
+    }
+
+
+}
